c77bef273fb9c6bd2d275149f767797227ea5fe1
ABsaneyra <absaneyra21@gmail.com>
2026-04-28 10:38:21 -0700

    Implement file upload feature in Flask app
    Added functionality to handle file uploads and display uploaded image.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Abraham Sanchez-Pereyra
-# 4/22/26
+# 4/28/26
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 import os
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-# Ensure an upload folder exists
+# You guys need an upload folder within your project in file explorer
 UPLOADS = 'static/uploads'
 if not os.path.exists(UPLOADS):
     os.makedirs(UPLOADS)
@@ -16,12 +16,16 @@ app.config['UPLOADS'] = UPLOADS
 
 @app.route('/', methods=['GET', 'POST'])
 def im():
-    # If the user uploads a file
-    if request.method == 'POST':
-        # Logic for saving file will go here later
-        pass
+    image_name = None 
     
-    return render_template('index.html')
-
+    if request.method == 'POST':
+        file = request.files.get('image_file')
+        
+        if file and file.filename != '':
+            path = os.path.join(app.config['UPLOADS'], file.filename)
+            file.save(path)
+            image_name = file.filename
+    
+    return render_template('index.html', image_name=image_name)
 if __name__ == "__main__":
     app.run(debug=True)
