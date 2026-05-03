32281c70ef24775d00fc88513312b91ab9ed062f
Eleanor Sanchez <eleanorsanchezmail@gmail.com>
2026-05-02 17:26:05 -0700

    optimized chroma key algorithm

--- chromakey.py
+++ chromakey.py
@@ -15,31 +15,33 @@ setattr(numpy, "asscalar", patch_asscalar)
 app = Flask(__name__)
 bs = Bootstrap5(app)
 
-def chromakey(src: Image.Image, replace: Image.Image, chroma_color: tuple[int, int, int], save_path: str = None): # pyright: ignore[reportArgumentType]
+def chromakey(original: Image.Image, new_background: Image.Image, chroma_color: tuple[int, int, int], save_path: str = None): # pyright: ignore[reportArgumentType]
     # Changes size of images we're working on. Reduce for performance, max should be 1440x1440. Bigger = waste.
     # The heat death of the universe will occur before my laptop finishes with anything above 256x256.
-    replace = replace.resize((256, 256))
-    src = src.resize(replace.size)
+    # new_background = new_background.resize((256, 256))
+    original = original.resize(new_background.size)
     
-    for x in range(src.width):
-        for y in range(src.height):
-            cur_pixel = src.getpixel((x,y))
+    orig_pixels = list(original.get_flattened_data())
+    new_pixels = list(new_background.get_flattened_data())
+
+    chroma_lab = convert_color(sRGBColor(chroma_color[0], chroma_color[1], chroma_color[2]), LabColor)
+
+    for pix_index in range(len(orig_pixels)):
+        cur_pixel = convert_color(sRGBColor(orig_pixels[pix_index][0], orig_pixels[pix_index][1], orig_pixels[pix_index][2]), LabColor) # pyright: ignore[reportIndexIssue]        
+        
+        if delta_e_cie2000(cur_pixel, chroma_lab) < 30:
+            orig_pixels[pix_index] = new_pixels[pix_index]
             
-            # RGB to lab for the colormath stuff.
-            cur_pixel = convert_color(sRGBColor(cur_pixel[0], cur_pixel[1], cur_pixel[2]), LabColor) # pyright: ignore[reportIndexIssue, reportOptionalSubscript]
-            chroma_lab = convert_color(sRGBColor(chroma_color[0], chroma_color[1], chroma_color[2]), LabColor)
-            
-            if delta_e_cie2000(cur_pixel, chroma_lab) < 77:
-                src.putpixel((x,y), replace.getpixel((x,y))) # pyright: ignore[reportArgumentType]
- 
-    src.show()
+    original.putdata(orig_pixels) #pyright: ignore[reportArgumentType]
+    # original.show()
+    
     if(save_path != None):
-        src.save(save_path)
-    
-    return src
+        original.save(save_path)
+        
+    return original
 
 # chromakey(Image.open("Space Desert.jpg"), Image.open("Eleanor Colorful.png"), (29, 40, 58), "chromafied.png")
 @app.route("/eleanor.html")
 def eleanorPage():
-    chromakey(Image.open("./static/images/Space Desert.jpg"), Image.open("./static/images/Eleanor Colorful.png"), (29, 40, 58), "chromafied.png");
+    chromakey(Image.open("./static/images/Space Desert.jpg"), Image.open("./static/images/Eleanor Colorful.png"), (29, 40, 58), "./static/images/chromafied.png");
     return render_template("eleanor.html")
